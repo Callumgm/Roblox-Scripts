05ce299577bb6a953cd5987282261af0db8993e9
CookiesKush420 <99442285+Callumgm@users.noreply.github.com>
2022-10-26 05:08:45 +0000

    Updates

--- assets/ui.lua
+++ assets/ui.lua
@@ -4846,17 +4846,7 @@ function library:CreateSettingsTab(menu)
                     end;
                 end;
                 setByPreset = false
-            end}):Select("Default");
-        
-            for i, v in pairs(library.theme) do
-                theme_section:AddColor({text = i, flag = i, color = library.theme[i], callback = function(c3)
-                    library.theme[i] = c3;
-                    library:SetTheme(library.theme);
-                    if not setByPreset and not setByConfig then 
-                        library.options.preset_theme:Select("Custom");
-                    end;
-                end});
-            end;
+            end}):Select("Twitch");
         end;
         -- 
         local configuration_section = settings:AddSection("Configuration", 2); do
